f318f8394bd561d3f3d2cdadd75c27a7cd1c456b
Eddie Ke <otienoaudrey1@gmail.com>
2023-04-11 13:45:50 +0300

    text

--- lib/screens/services/recommended_list.dart
+++ lib/screens/services/recommended_list.dart
@@ -3,6 +3,7 @@ import 'package:niva/utilities/colors.dart';
 import 'package:niva/utilities/dimensions.dart';
 import 'package:niva/widgets/big_text.dart';
 import 'package:niva/widgets/app_icons.dart';
+import 'package:niva/widgets/expandable_text_widget.dart';
 
 class RecommendedList extends StatelessWidget {
   const RecommendedList({super.key});
@@ -56,8 +57,15 @@ class RecommendedList extends StatelessWidget {
         ),
       ),
       SliverToBoxAdapter(
-        child: Text(
-            "Introducing our new lipstick product, perfect for any occasion! Our creamy and long-lasting formula glides on smoothly, delivering a vibrant and rich color that lasts for hours. The luxurious finish is both hydrating and nourishing, leaving your lips feeling soft and supple. With a wide range of shades to choose from, there's a color for everyone. Whether you're looking for a bold red or a subtle nude, this lipstick is sure to become your go-to choice. Plus, with our easy-to-use ecommerce app, ordering and receiving your new favorite lipstick has never been easier. Get ready to elevate your makeup game with our fabulous new lipstick!"),
+        child: Column(
+          children: [
+            Container(
+              child: ExpandableTextWidget(
+                  text:
+                      "Introducing our new lipstick product, perfect for any occasion! Our creamy and long-lasting formula glides on smoothly, delivering a vibrant and rich color that lasts for hours. The luxurious finish is both hydrating and nourishing, leaving your lips feeling soft and supple. With a wide range of shades to choose from, there's a color for everyone. Whether you're looking for a bold red or a subtle nude, this lipstick is sure to become your go-to choice. Plus, with our easy-to-use ecommerce app, ordering and receiving your new favorite lipstick has never been easier. Get ready to elevate your makeup game with our fabulous new lipstick!"),
+            )
+          ],
+        ),
       ),
     ]));
   }
