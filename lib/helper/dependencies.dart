c85929be013351ed3ba9eb333c9d8eb7389651d6
Eddie Ke <otienoaudrey1@gmail.com>
2023-04-28 00:37:13 +0300

    dependencies/ repositories/ api client/ Controllers

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,1 +1,16 @@
+import 'package:get/get.dart';
+import 'package:niva/data/api/api_client.dart';
+import 'package:niva/data/repository/products_services_repo.dart';
+import 'package:niva/Controllers/popular_products_controller.dart';
 
+Future<void> init() async {
+  //api client
+  Get.lazyPut(() => ApiClient(appBaseUrl: "https//www.niva.com"));
+
+  //repos
+  Get.lazyPut(() => ProductsServicesRepo(apiClient: Get.find()));
+
+  //controllers
+  Get.lazyPut(
+      () => PopularProductsController(productsServicesRepo: Get.find()));
+}
