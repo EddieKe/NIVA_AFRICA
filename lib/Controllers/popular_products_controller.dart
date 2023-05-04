11f2a03a46097e82516c7d24192cfc263d0dd0bd
Eddie Ke <otienoaudrey1@gmail.com>
2023-05-04 18:13:33 +0300

    product module

--- lib/Controllers/popular_products_controller.dart
+++ lib/Controllers/popular_products_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:niva/data/repository/products_services_repo.dart';
+import 'package:niva/Models/products_model.dart';
 
 class PopularProductsController extends GetxController {
   final ProductsServicesRepo productsServicesRepo;
@@ -12,7 +13,7 @@ class PopularProductsController extends GetxController {
     Response response = await productsServicesRepo.getProductsServicesList();
     if (response.statusCode == 200) {
       _productsServicesList = [];
-      // _productsServicesList.addAll();
+      _productsServicesList.addAll(Product.fromJson(response.body).products);
       update();
     } else {}
   }
