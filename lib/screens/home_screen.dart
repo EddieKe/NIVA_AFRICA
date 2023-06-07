203569ea446b89b350c531613c482c351f10f4fb
Eddie Ke <otienoaudrey1@gmail.com>
2023-06-07 12:00:02 +0300

    navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:niva/components/home/body.dart';
+import 'package:niva/components/main_components.dart/custom_nav.dart';
+import 'package:niva/components/main_components.dart/enums.dart';
 
 class HomeScreen extends StatefulWidget {
   static String routeName = "/home";
@@ -11,6 +14,11 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: const Column());
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Body(),
+      bottomNavigationBar:
+          const CustomBottomNavBar(selectedMenu: MenuState.home),
+    );
   }
 }
