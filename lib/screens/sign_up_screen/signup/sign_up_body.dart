424aeb0b695738e747e4ac86e48d8c59dfe945b9
Wagah <otienoaudrey1@gmail.com>
2023-08-14 04:45:35 -0700

    rendered social media logins

--- lib/screens/sign_up_screen/signup/sign_up_body.dart
+++ lib/screens/sign_up_screen/signup/sign_up_body.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:niva/components/main_components.dart/socal_card.dart';
+// import 'package:niva/components/main_components.dart/socal_card.dart';
 import 'package:niva/utilities/constants.dart';
 import 'package:niva/utilities/dimensions2.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'sign_up_form.dart';
+class SocalCard extends StatelessWidget {
+  final String icon;
+  final Function press;
+  const SocalCard({Key? key, required this.icon, required this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => press(),
+      child: SizedBox(
+        height: getProportionateScreenHeight(30),
+        width: getProportionateScreenWidth(30),
+        child: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -28,16 +47,26 @@ class Body extends StatelessWidget {
                 SizedBox(height: Dimensions2.screenHeight * 0.08),
                 const SignUpForm(),
                 SizedBox(height: Dimensions2.screenHeight * 0.08),
-                // ignore: avoid_unnecessary_containers
+                
                 Container(
                   padding: const EdgeInsets.all(8.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       SocalCard(
-                        icon: "assets/icons/google-icon.svg",
-                        press: () {},
-                      ),
+                          icon: "assets/image/icons/google-icon.svg",
+                          press: () {},
+                        ),
+                        SizedBox(width: getProportionateScreenWidth(50)),
+                        SocalCard(
+                          icon: "assets/image/icons/apple-icon.svg",
+                          press: () {},
+                        ),
+                        SizedBox(width: getProportionateScreenWidth(50)),
+                        SocalCard(
+                          icon: "assets/image/icons/twitter-icon.svg",
+                          press: () {},
+                        ),
                     ],
                   ),
                 ),
