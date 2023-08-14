25ef661f25934d391a126503b577a9eea2b23daf
Wagah <otienoaudrey1@gmail.com>
2023-08-14 04:44:45 -0700

    fixed render flow error

--- lib/screens/login_screen/login/login_body.dart
+++ lib/screens/login_screen/login/login_body.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:niva/components/main_components.dart/no_account_text.dart';
-import 'package:niva/components/main_components.dart/socal_card.dart';
 import 'package:niva/utilities/dimensions2.dart';
 import 'package:niva/screens/login_screen/login/login_form.dart';
-// import 'package:sign_in_with_apple/sign_in_with_apple.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+class SocalCard extends StatelessWidget {
+  final String icon;
+  final Function press;
+  const SocalCard({Key? key, required this.icon, required this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => press(),
+      child: SizedBox(
+        height: getProportionateScreenHeight(30),
+        width: getProportionateScreenWidth(30),
+        child: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
 
 class Body extends StatelessWidget {
-  const Body({super.key});
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,55 +47,44 @@ class Body extends StatelessWidget {
                     fontWeight: FontWeight.w500,
                   ),
                 ),
-                const Text(
-                  "Sign in with your email and password  \nor continue with social media",
-                  textAlign: TextAlign.center,
-                ),
+                const Text("You have been missed",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                    )),
                 SizedBox(height: Dimensions2.screenHeight * 0.08),
                 const LoginForm(),
                 SizedBox(height: Dimensions2.screenHeight * 0.08),
-                Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: getProportionateScreenHeight(140),
-                  width: getProportionateScreenHeight(50),
-                  decoration: BoxDecoration(
-                    borderRadius:
-                        BorderRadius.circular(getProportionateScreenHeight(20)),
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SocalCard(
-                            icon: "assets/icons/google-icon.svg",
-                            press: () {},
-                          ),
-                          const Text("Sign in with Google")
-                        ],
-                      ),
-                      SizedBox(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              // Handle Apple Sign In
-                            },
-                            child: Row(
-                              children: [
-                                SvgPicture.asset(
-                                    "assets/icons/apple-icon.svg"), // Apple SVG icon
-                                const Text("Sign in with Apple")
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    height: getProportionateScreenHeight(80),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                          getProportionateScreenHeight(100)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SocalCard(
+                          icon: "assets/image/icons/google-icon.svg",
+                          press: () {},
+                        ),
+                        SizedBox(width: getProportionateScreenWidth(50)),
+                        SocalCard(
+                          icon: "assets/image/icons/apple-icon.svg",
+                          press: () {},
+                        ),
+                        SizedBox(width: getProportionateScreenWidth(50)),
+                        SocalCard(
+                          icon: "assets/image/icons/twitter-icon.svg",
+                          press: () {},
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(height: getProportionateScreenHeight(20)),
+                SizedBox(height: getProportionateScreenHeight(10)),
                 const NoAccountText(),
               ],
             ),
