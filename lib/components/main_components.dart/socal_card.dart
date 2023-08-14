00cc4c3c7798faef97f340984d3daa2c337999bb
Wagah <otienoaudrey1@gmail.com>
2023-08-14 04:44:19 -0700

    fixed svg renderer issues

--- lib/components/main_components.dart/socal_card.dart
+++ lib/components/main_components.dart/socal_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:niva/utilities/dimensions2.dart';
 
@@ -27,7 +27,7 @@ class SocalCard extends StatelessWidget {
           color: Color(0xFFF5F6F9),
           shape: BoxShape.circle,
         ),
-        //  child: SvgPicture.asset(icon!),
+          child: SvgPicture.asset(icon!),
       ),
     );
   }
