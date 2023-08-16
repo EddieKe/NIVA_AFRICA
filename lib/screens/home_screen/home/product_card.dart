af3e6e95ad9491d71484c7d96f3a3921855eba52
Wagah <otienoaudrey1@gmail.com>
2023-08-16 07:49:01 -0700

    restyled the product_card

--- lib/screens/home_screen/home/product_card.dart
+++ lib/screens/home_screen/home/product_card.dart
@@ -1,3 +1,5 @@
+// ProductCard.dart
+
 import 'package:flutter/material.dart';
 import 'package:niva/models/product.dart';
 import 'package:niva/screens/details%20screen/details_screen.dart';
@@ -5,13 +7,12 @@ import 'package:niva/utilities/constants.dart';
 import 'package:niva/utilities/dimensions2.dart';
 
 class ProductCard extends StatelessWidget {
-  final double width, aspectRatio;
+  final double width;
   final Product product;
 
   const ProductCard({
     Key? key,
-    this.width = 140,
-    this.aspectRatio = 1.02,
+    this.width = 160, // Increased the default width
     required this.product,
   }) : super(key: key);
 
@@ -19,8 +20,12 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
-      child: SizedBox(
+      child: Container(
         width: getProportionateScreenWidth(width),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade200), // Added border
+          borderRadius: BorderRadius.circular(15),
+        ),
         child: GestureDetector(
           onTap: () => Navigator.pushNamed(
             context,
@@ -31,16 +36,19 @@ class ProductCard extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               AspectRatio(
-                aspectRatio: aspectRatio,
+                aspectRatio: 1.02,
                 child: Container(
-                  padding: EdgeInsets.all(getProportionateScreenWidth(20)),
+                  padding: EdgeInsets.all(getProportionateScreenWidth(0)),
                   decoration: BoxDecoration(
-                    color: nSecondaryColor.withOpacity(0.1),
                     borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: AssetImage(product.images[0]),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                   child: Hero(
                     tag: product.id.toString(),
-                    child: Image.asset(product.images[0]),
+                    child: Container(),
                   ),
                 ),
               ),
@@ -49,6 +57,16 @@ class ProductCard extends StatelessWidget {
                 product.title,
                 style: const TextStyle(color: Colors.black),
                 maxLines: 2,
+              ),
+              Text(
+                // Description added here
+                product.description,
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.6),
+                  fontSize: 12,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -83,6 +101,15 @@ class ProductCard extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+              const Text(
+                "order Now",
+                style: TextStyle(
+                  fontSize: 13,
+                  fontFamily: "Poppins", // Typo fixed
+                  color: Color(0xFF964B00), // Hex color code added
+                  fontWeight: FontWeight.w200,
+                ),
               )
             ],
           ),
