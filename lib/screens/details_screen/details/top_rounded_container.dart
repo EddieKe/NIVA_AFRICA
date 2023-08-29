22e154ea792e8d4b5d113859a70ced70e0306870
Wagah <otienoaudrey1@gmail.com>
2023-08-29 11:04:26 -0700

    modified

--- lib/screens/details_screen/details/top_rounded_container.dart
+++ lib/screens/details_screen/details/top_rounded_container.dart
@@ -15,14 +15,14 @@ class TopRoundedContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: getProportionateScreenWidth(20)),
-      padding: EdgeInsets.only(top: getProportionateScreenWidth(20)),
+      margin: EdgeInsets.only(top: getProportionateScreenWidth(10)),
+      padding: EdgeInsets.only(top: getProportionateScreenWidth(10)),
       width: double.infinity,
       decoration: BoxDecoration(
         color: color,
         borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40),
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
         ),
       ),
       child: child,
--- lib/screens/home_screen/home/home_body.dart
+++ lib/screens/home_screen/home/home_body.dart
@@ -2,64 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:niva/screens/home_screen/home/custom_banner.dart';
 import 'package:niva/widgets/categories.dart';
 import 'special_offers.dart';
-import 'popular_product.dart'; // Ensure this is the correct import for PopularProducts
-import 'popular_services.dart'; // Ensure this is the correct import for PopularServices
-import 'package:niva/models/product.dart';
-import 'package:niva/models/service.dart';
+import 'popular_product.dart';
+import 'popular_services.dart';
+
 import 'package:niva/utilities/dimensions2.dart';
+import 'package:niva/data/data_provider.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Example data for the products
-    final List<Product> productsList = [
-      Product(
-        id: 1,
-        title: "Laroche Posay Moisturizer",
-        price: 2250,
-        images: ["assets/image/laroche3.jpg"],
-        isFavourite: false,
-        description: 'Mositurize your face with our Laroche Moisturizer',
+    final productsList = DataProvider.getProducts();
+    final servicesList = DataProvider.getServices();
 
-        // Other fields here...
-      ),
-      Product(
-        id: 2,
-        title: "Lipstick",
-        price: 1500,
-        images: ["assets/image/lipstick2.jpg"],
-        isFavourite: true, description: '',
-        // Other fields here...
-      ),
-      // Add more products if needed
-    ];
-
-    // Example data for the services
-    final List<Service> servicesList = [
-      Service(
-        id: 1,
-        title: "Artificial Locs",
-        price: 2500,
-        images: ["assets/image/locs.jpg"],
-        description: 'Articila locs are protective hairstyle ',
-        location: 'Nairobi MAchakos',
-        provider: 'Eddie Makeup',
-        isFavourite: true,
-      ),
-      Service(
-        id: 2,
-        title: "Nails",
-        price: 1500,
-        images: ["assets/image/nail2.jpg"],
-        provider: 'Stylish nails',
-        description:
-            'Beautifully and well crafted nails.This a well work of Tips and Nails',
-        location: 'Nairobi Cbd',
-        isFavourite: true,
-      ),
-    ];
+  
 
     return SafeArea(
       child: SingleChildScrollView(
